4e14c830c7fdfa1091f1e96c0825efbb68efb10e
MATTHEW DIGISO <126MDIGISO@FRHSDCOM>
2025-03-18 14:00:32 -0400

    commit and push changes

--- js/index.js
+++ js/index.js
@@ -1,39 +1,164 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-
-class Boundary {
-  constructor({ position, width, height }) {
-    this.position = position
-    this.width = width
-    this.height = height
-  }
-
-  draw() {
-    c.fillStyle = 'blue'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-  }
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+console.log(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+window.onresize = function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
-const map = [
-    ['_', '-', '-', '-', '-', '-', '-', '-', '-'],
-    ['|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['|', '_', '_', '_', '_', '_', '_', '_', '_']
-]
+class Boundary {
+    static width = 40
+    static height = 40
+    constructor({position}){
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+
+    draw(){
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+class Player {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 10
+    }
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2,)
+       c.fillStyle = 'yellow'
+       c.fill()
+        c.closePath()
+    }
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
 
 const boundaries = [];
+const player = new Player({
+    position: {
+        x:Boundary.width+ Boundary.width/2,
+        y:Boundary.height+Boundary.height/2
+    },
+    velocity:{
+        x:0,
+        y:0
+    }
+})
+const keys = {
+    w:{
+        pressed: false
+    },
+    a:{
+        pressed: false
+    },
+    s:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    }
+    
+    
+}
 
-map.forEach(row, i) => 
-    row.forEach[(symbol, j) => {
-        if (symbol !== ' ') {
-            const width = 40
-            const height = 40
-            const x = j * width
-            const y = i * height
-            boundaries.push(new Boundary({ position: { x, y }, width, height }))
+let lastKey = ''
+
+
+const map = [
+    ['-','-','-','-','-','-'],
+    ['-',' ',' ',' ',' ','-'],
+    ['-',' ','-','-',' ','-'],
+    ['-',' ',' ',' ',' ','-'],
+    ['-','-','-','-','-','-']
+]
+
+
+map.forEach((row, i) => {
+    row.forEach((Symbol, j) => {
+        switch(Symbol){
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: Boundary.width * j, 
+                            y: Boundary.height * i
+                        }
+                    }))
+                break;
         }
-    }]
+    })
+})
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    boundaries.forEach((boundary) => {
+        boundary.draw();
+        
+    })
+    
+    player.update();
+    player.velocity.x=0;
+    player.velocity.y=0;
+
+    if(keys.w.pressed && lastKey === 'w'){
+        player.velocity.y = -5;
+    }else if(keys.a.pressed&& lastKey === 'a'){
+        player.velocity.x=-5
+    }else if(keys.s.pressed&& lastKey === 's'){
+        player.velocity.y=5
+    }else if(keys.d.pressed&& lastKey === 'd'){
+        player.velocity.x=5
+    }
+}
+animate();
+
+
+addEventListener('keydown',({key})=>{
+    switch(key){
+        case 'w':
+            keys.w.pressed = true;
+            lastKey = 'w'
+        break
+        case 'a':
+            keys.a.pressed = true;
+            lastKey = 'a'
+        break
+        case 's':
+            keys.s.pressed = true;
+            lastKey = 's'
+        break
+        case 'd':
+            keys.d.pressed = true;
+            lastKey = 'd'
+        break
+    }
+})
+addEventListener('keyup',({key})=>{
+    switch(key){
+        case 'w':
+            keys.w.pressed = false;        
+        break
+        case 'a':
+            keys.a.pressed = false;  
+        break
+        case 's':
+            keys.s.pressed = false;  
+        break
+        case 'd':
+            keys.d.pressed = false;  
+        break
+    }
+
+
+})
